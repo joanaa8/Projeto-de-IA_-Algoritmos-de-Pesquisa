7413beaa15f86d915f593b8b8d452d0b763d76b4
Joana Teixeira <isabelkl2004@gmail.com>
2025-12-15 01:01:50 +0000

    commit

--- gerar_dataset.py
+++ gerar_dataset.py
@@ -2,11 +2,11 @@ import pandas as pd
 import random
 
 def gerar_dataset_30_navios():
-    # Mudei o nome para não apagar o teu ficheiro de 100 navios
+    
     filename = "dataset_navios_porto_100.xlsx" 
     num_navios = 100  # O limite que queremos testar
     
-    # --- Configurações de Realismo ---
+    
     portos_origem = ["Roterdão", "Xangai", "Singapura", "Los Angeles", "Hamburgo", "Santos", "Lisboa", "Sines"]
     
     cargas_grande_porte = ["Contentores", "Passageiros (Cruzeiro)", "Gás Natural", "Petróleo"]
@@ -17,15 +17,15 @@ def gerar_dataset_30_navios():
 
     print(f"A gerar '{filename}' com {num_navios} navios...")
 
-    # CORREÇÃO AQUI: Usar num_navios + 1 em vez de 101
+    
     for i in range(1, num_navios + 1):
         id_navio = f"N{i:03d}"
         
-        # 30% Grande Porte (Zona A), 70% Médio (Zona B/A)
+        
         is_grande_porte = random.random() < 0.3 
         
         if is_grande_porte:
-            # --- ZONA A: Navios de Grande Porte ---
+            
             tipo = "Tipo 2"
             zona = "A"
             tipo_carga = random.choice(cargas_grande_porte)
@@ -33,7 +33,7 @@ def gerar_dataset_30_navios():
             prioridade = random.randint(3, 5)
             
         else:
-            # --- ZONA B: Navios de Médio/Pequeno Porte ---
+            
             tipo = "Tipo 1"
             zona = "A, B"
             tipo_carga = random.choice(cargas_medio_porte)
